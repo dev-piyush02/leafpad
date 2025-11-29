3e813ca905c7e30b9904f72b1b3d989b13a435d9
Piyush <mailpiyush02@yahoo.com>
2025-11-29 10:24:24 +0530

    resolved application.yml vulnerabilities

--- src/main/java/com/notes/theidlenotes/TheidlenotesApplication.java
+++ src/main/java/com/notes/theidlenotes/TheidlenotesApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TheidlenotesApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TheidlenotesApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(TheidlenotesApplication.class, args);
 	}
 
 }
